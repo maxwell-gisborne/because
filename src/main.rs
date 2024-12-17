5a8d2e9142a2c3b774d587afe8c9a9ca985cb364
maxwell <maxwell.gisborne@plymouth.ac.uk>
2024-12-17 17:10:01 +0000

    server and client

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncWriteExt,AsyncReadExt};
 use std::io;
-use std::io::{Result, Error};
+use std::io::Result;
 use std::string::String;
 
 async fn process_socket(mut stream: TcpStream) -> Result<String> {
@@ -13,14 +13,34 @@ async fn process_socket(mut stream: TcpStream) -> Result<String> {
     Ok(string)
 }
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
+async fn run_server() -> Result<()>{
     let listener = TcpListener::bind("0.0.0.0:8080").await?;
-    println!("Just Because");
+    println!("Runing server");
     loop {
         let (stream, address) = listener.accept().await?;
         println!("Connected {}", address);
         let result = process_socket(stream).await.unwrap();
         println!("{}", result);
+    }
+}
+
+enum ServerFoundStatus {
+    Found,
+    No,
+}
+
+async fn run_client() -> Result<ServerFoundStatus> {
+    match TcpStream::connect("127.0.0.1:8080").await {
+        Err(_) => {return Ok(ServerFoundStatus::No);},
+        Ok(stream) => {println!("{}", process_socket(stream).await.unwrap());},
+    };
+    Ok(ServerFoundStatus::Found)
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    match run_client().await.unwrap() {
+        ServerFoundStatus::No => run_server().await,
+        ServerFoundStatus::Found => Ok(())
     }
 }
