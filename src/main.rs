424d0453aaecd9b269cc636fe3c627133c5ee47a
maxwell <maxwell.gisborne@plymouth.ac.uk>
2024-12-17 16:44:57 +0000

    shit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use tokio::net::{TcpListener, TcpStream};
-use tokio::io::AsyncWriteExt;
+use tokio::io::{AsyncWriteExt,AsyncReadExt};
 use std::io;
+use std::io::{Result, Error};
+use std::string::String;
 
-async fn process_socket(mut stream: TcpStream) {
-    stream.write_all(b"Just Because!\n").await;
+async fn process_socket(mut stream: TcpStream) -> Result<String> {
+    let _res = stream.write_all(b"Just Because!\n").await?;
+    stream.readable().await?;
+    let mut buffer = [0; 100];
+    let _bytes_read = stream.read(&mut buffer).await?;
+    let string = String::from_utf8_lossy(&buffer).to_owned().to_string();
+    Ok(string)
 }
 
 #[tokio::main]
@@ -12,6 +19,8 @@ async fn main() -> io::Result<()> {
     println!("Just Because");
     loop {
         let (stream, address) = listener.accept().await?;
-        process_socket(stream).await;
+        println!("Connected {}", address);
+        let result = process_socket(stream).await.unwrap();
+        println!("{}", result);
     }
 }
